ef1fc006f9e5eb08bd017abba0106bdb419a4365
Mark Lindhout <mark@twb.berlin>
2019-06-16 16:23:56 +0200

    Added a bunch of web sites and pages to block. Man, there's some wacky stuff out there!

--- src/js/common/pagelist.js
+++ src/js/common/pagelist.js
@@ -2,15 +2,30 @@
 
 var hetzblocker = hetzblocker || {} // eslint-disable-line no-use-before-define
 hetzblocker.common = hetzblocker.common || {}
-hetzblocker.common.pagelist = (function () {
-  return {
-    list: [
-      'http://facebook.com/bild',
-      'http://facebook.com/BildSaarland',
-      'http://facebook.com/BILD.Mallorca',
-      'https://twitter.com/bild',
-      'https://twitter.com/BILD_aktuell',
-      'http://facebook.com/telegraaf'
-    ]
+hetzblocker.common.pagelist = (
+  function () {
+    return {
+      list: [
+        'http://facebook.com/bild',
+        'http://facebook.com/BILD.Mallorca',
+        'http://facebook.com/BildSaarland',
+        'http://facebook.com/telegraaf',
+        'http://juergenelsaesser.wordpress.com',
+        'http://quotenqueen.wordpress.com',
+        'http://robertknoche.wordpress.com',
+        'http://schluesselkindblog.wordpress.com',
+        'http://volksbetrugpunktnet.wordpress.com',
+        'http://neokonservativ.wordpress.com',
+        'https://twitter.com/bild ',
+        'https://twitter.com/BILD_aktuell',
+        'https://www.youtube.com/channel/UCdroYqaHMrTY1feMaPP_u0g',
+        'https://www.youtube.com/user/FPOETVonline',
+        'https://www.youtube.com/user/svpch',
+        'https://www.youtube.com/user/iprotestblog',
+        'https://www.youtube.com/channel/UCGy6uV7yqGWDeUWTZzT3ZEg',
+        'https://www.youtube.com/user/PragerUniversity',
+        'https://www.reddit.com/r/TheRedPill/'
+      ]
+    }
   }
-})()
+)()
